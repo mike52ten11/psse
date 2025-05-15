a3d89c5917314eb783e1cadce1b9190389648079
mike52ten11 <85476871+mike52ten11@users.noreply.github.com>
2025-05-15 09:03:52 +0800

    Delete pssefunction directory

--- pssefunction/pssefunctionsrc/src/base/check_encoding_type_from_psse.py
+++
@@ -1,6 +0,0 @@
-import chardet
-
-
-def check_encoding_type(checked_data):
-     
-    return chardet.detect(checked_data)['encoding']
--- pssefunction/pssefunctionsrc/src/base/run_pyfile_by_execmd.py
+++
@@ -1,8 +0,0 @@
-
-
-
-
-
-def Run_pyfile_by_execmd(python_location,pyfile,args):
-
-    return f"{python_location} {pyfile} {args}"
